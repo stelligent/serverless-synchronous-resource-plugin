4d26725ef2a6ec83809be25ac74a8b50c4c4f03b
David Bettinger <dave.bettinger@stelligent.com>
2016-04-01 17:07:03 -0400

    added cli-spinner and usage examples

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# serverless-synchronous-resource-plugin
+## serverless-synchronous-resource-plugin
 
 Serverless plugin for synchronously deploying CFN stacks with monitoring of stack events.
+
+**Usage**
+
+This plugin provides a new Serverless Actions which can be invoked via the Serverless CLI from a project root directory.
+
+```
+# Create or upate a CFN stack
+==> synchronousResources deploy -t relative/path/to/a/cfn/template.json -s prod -r us-east-1
+```
+
+```
+# Delete a CFN stack
+==> synchronousResources remove -t relative/path/to/a/cfn/template.json -s prod -r us-east-1
+```
+
+
--- index.js
+++ index.js
@@ -1,114 +1,134 @@
 'use strict';
 
 const
-  BbPromise = require('bluebird'),
-  CFNRunner = require('./lib/cfnRunner');
+    BbPromise = require('bluebird'),
+    CFNRunner = require('./lib/cfnRunner'),
+    chalk = require('chalk'),
+    Spinner = require('cli-spinner').Spinner;
 
-module.exports = function(S) { // Always pass in the ServerlessPlugin Class
 
-  class SynchronousResource extends S.classes.Plugin {
+module.exports = function (S) { // Always pass in the ServerlessPlugin Class
 
-    constructor() {
-      super();
-      this.name = 'customResource'; // Define your plugin's name
+
+    class SynchronousResource extends S.classes.Plugin {
+
+        constructor() {
+            super();
+            this.name = 'customResource'; // Define your plugin's name
+        }
+
+        registerActions() {
+
+            S.addAction(this._deployResources.bind(this), {
+                handler: 'deployResources',
+                description: 'A custom action from a custom plugin',
+                context: 'synchronousResources',
+                contextAction: 'deploy',
+                options: [
+                    {
+                        option: 'stage',
+                        shortcut: 's',
+                        description: 'stage you want to deploy'
+                    },
+                    {
+                        option: 'region',
+                        shortcut: 'r',
+                        description: 'region in which you want to deploy'
+                    },
+                    {
+                        option: 'templatePath',
+                        shortcut: 't',
+                        description: 'CFN stack you want to deploy'
+                    }
+                ],
+                parameters: []
+            });
+
+            S.addAction(this._removeResources.bind(this), {
+                handler: 'removeResources',
+                description: 'A custom action from a custom plugin',
+                context: 'synchronousResources',
+                contextAction: 'remove',
+                options: [
+                    {
+                        option: 'stage',
+                        shortcut: 's',
+                        description: 'stage you want to remove'
+                    },
+                    {
+                        option: 'region',
+                        shortcut: 'r',
+                        description: 'region from which you want to remove'
+                    },
+                    {
+                        option: 'templatePath',
+                        shortcut: 't',
+                        description: 'CFN stack you want to delete'
+                    }
+                ],
+                parameters: []
+            });
+
+            return BbPromise.resolve();
+        }
+
+
+        _deployResources(evt) {
+
+            let _this = this;
+
+            return new BbPromise(function (resolve, reject) {
+
+                var cfnRunner = new CFNRunner(evt.options.region, evt.options.templatePath);
+                var cb = function (err) {
+                    spinner.stop(true);
+                    if (err) {
+                        console.log(err);
+                        return reject(evt);
+                    }
+                };
+
+                var spinner = new Spinner('  ' + chalk.yellow('%s '));
+                spinner.setSpinnerDelay(60);
+                spinner.start();
+
+                cfnRunner.deployStack(cb);
+
+                return resolve(evt);
+
+            });
+        }
+
+        _removeResources(evt) {
+
+            let _this = this;
+
+            return new BbPromise(function (resolve, reject) {
+
+                var cfnRunner = new CFNRunner(evt.options.region, evt.options.templatePath);
+                var cb = function (err) {
+                    spinner.stop(true);
+                    if (err) {
+                        console.log(err);
+                        return reject(evt);
+                    }
+                };
+
+                var spinner = new Spinner('  ' + chalk.yellow('%s '));
+                spinner.setSpinnerDelay(60);
+                spinner.start();
+
+                cfnRunner.deleteStack(cb);
+
+                return resolve(evt);
+
+            });
+        }
+
+
     }
 
-    registerActions() {
-
-      S.addAction(this._deployResources.bind(this), {
-        handler:       'deployResources',
-        description:   'A custom action from a custom plugin',
-        context:       'synchronousResources',
-        contextAction: 'deploy',
-        options:       [
-          {
-            option:      'stage',
-            shortcut:    's',
-            description: 'stage you want to deploy'
-          },
-          {
-            option:      'region',
-            shortcut:    'r',
-            description: 'region in which you want to deploy'
-          },
-          {
-            option:      'templatePath',
-            shortcut:    't',
-            description: 'CFN stack you want to deploy'
-          }
-        ],
-        parameters: []
-      });
-
-      S.addAction(this._removeResources.bind(this), {
-        handler:       'removeResources',
-        description:   'A custom action from a custom plugin',
-        context:       'synchronousResources',
-        contextAction: 'remove',
-        options:       [
-          {
-            option:      'stage',
-            shortcut:    's',
-            description: 'stage you want to remove'
-          },
-          {
-            option:      'region',
-            shortcut:    'r',
-            description: 'region from which you want to remove'
-          },
-          {
-            option:      'templatePath',
-            shortcut:    't',
-            description: 'CFN stack you want to delete'
-          }
-        ],
-        parameters: []
-      });
-
-      return BbPromise.resolve();
-    }
-
-
-    _deployResources(evt) {
-
-      return new BbPromise(function (resolve, reject) {
-
-        var cfnRunner = new CFNRunner(evt.options.region, evt.options.templatePath);
-        var cb = function(err){
-          if (err) {
-            console.log(err);
-            return reject(evt);
-          }
-        };
-        cfnRunner.deployStack(cb);
-
-        return resolve(evt);
-
-      });
-    }
-
-    _removeResources(evt) {
-
-      return new BbPromise(function (resolve, reject) {
-
-        var cfnRunner = new CFNRunner(evt.options.region, evt.options.templatePath);
-        var cb = function(err){
-          if (err) {
-            console.log(err);
-            return reject(evt);
-          }
-        };
-        cfnRunner.deleteStack(cb);
-
-        return resolve(evt);
-
-      });
-    }
-
-
-  }
-
-  // Export Plugin Class
-  return SynchronousResource;
+    // Export Plugin Class
+    return SynchronousResource;
 
 };
